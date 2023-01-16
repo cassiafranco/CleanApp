7a9bb343bd98ab11ce84d322ea497a80f1e04f71
Cassia <T3632860@timbrasil.com.brT3632860@timbrasil.com.br>
2023-01-16 11:36:56 -0300

    ensure AlamofireAdapter calls Alamofire with correct data

--- DataTests/Helpers/TestFactories.swift
+++ DataTests/Helpers/TestFactories.swift
@@ -3,6 +3,9 @@ import Foundation
 func makeInvalidData() -> Data {
     return Data("invalid_data".utf8)
 }
+func makeValidData() -> Data {
+    return Data("{\"name\":\"Rodrigo\"}".utf8)
+}
 func makeUrl() -> URL {
     return URL(string: "http://any-url.com")!
 }
