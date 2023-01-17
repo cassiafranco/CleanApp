684288fddbb9f013c81743e7e2e08a96418fcdf3
Cassia <T3632860@timbrasil.com.brT3632860@timbrasil.com.br>
2023-01-17 11:06:25 -0300

    add data extension to convert to json

--- Data/Helpers/ExtensionHelpers.swift
+++ Data/Helpers/ExtensionHelpers.swift
@@ -4,4 +4,7 @@ public extension Data {
     func toModel<T: Decodable>() -> T? {
         return try? JSONDecoder().decode(T.self, from: self)
     }
+    func toJSon() -> [String: Any]? {
+        return try? JSONSerialization.jsonObject(with: self, options: .allowFragments) as? [String: Any]
+    }
 }
